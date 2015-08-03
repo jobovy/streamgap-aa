2e7abf9856536f6a7b2b393a75b2c9228a5a011b
Jo Bovy <bovy@ias.edu>
2015-08-03 15:45:28 -0400

    update calculation

--- py/calc_actions.py
+++ py/calc_actions.py
@@ -6,7 +6,7 @@ import numpy
 from galpy import potential
 from galpy.util import bovy_coords
 from galpy.actionAngle import actionAngleIsochroneApprox
-def calc_actions(snapfile=None):
+def calc_actions(snapfile=None,first=0):
     #Setup potential and actionAngle object
     lp= potential.LogarithmicHaloPotential(normalize=1.,q=0.9)
     aA= actionAngleIsochroneApprox(pot=lp,b=0.8)
@@ -15,7 +15,7 @@ def calc_actions(snapfile=None):
     xv= xvid[:,:6]
     id= xvid[:,6]
     xv= xv[numpy.argsort(id)]
-    id= id[numpy.argsort(id)]
+    id= id[numpy.argsort(id)].astype(int)
     R,phi,Z= bovy_coords.rect_to_cyl(xv[:,0],xv[:,1],xv[:,2])
     vR,vT,vZ= bovy_coords.rect_to_cyl_vec(xv[:,3],xv[:,4],xv[:,5],R,phi,Z,
                                           cyl=True)
@@ -32,9 +32,29 @@ def calc_actions(snapfile=None):
         vZ= vZ[0:100]
         phi= phi[0:100]
     nx= len(R)
+    if first:
+        R= R[:nx//2]
+        vR= vR[:nx//2]
+        vT= vT[:nx//2]
+        Z= Z[:nx//2]
+        vZ= vZ[:nx//2]
+        phi= phi[:nx//2]
+        id= id[:nx//2]
+    else:
+        R= R[nx//2:]
+        vR= vR[nx//2:]
+        vT= vT[nx//2:]
+        Z= Z[nx//2:]
+        vZ= vZ[nx//2:]
+        phi= phi[nx//2:]
+        id= id[nx//2:]
+    nx/= 2
     #calculation actions, frequencies, and angles
     #Processes in batches to not run out of memory
-    csvfilename= snapfile.replace('.txt','_aA.txt')
+    if first:
+        csvfilename= snapfile.replace('.txt','_aA1.txt')
+    else:
+        csvfilename= snapfile.replace('.txt','_aA2.txt')
     if os.path.exists(csvfilename):
         #Don't recalculate those that have already been calculated
         nstart= int(subprocess.check_output(['wc','-l',csvfilename]).split(' ')[0])
@@ -63,11 +83,12 @@ def calc_actions(snapfile=None):
         for jj in range(len(tacfs[0])):
             writer.writerow([tacfs[0][jj],tacfs[1][jj],tacfs[2][jj],
                              tacfs[3][jj],tacfs[4][jj],tacfs[5][jj],
-                             tacfs[6][jj],tacfs[7][jj],tacfs[8][jj]])
+                             tacfs[6][jj],tacfs[7][jj],tacfs[8][jj],
+                             id[nstart+ii*nbatch+jj]])
             csvfile.flush()
     csvfile.close()
     return None
 
 
 if __name__ == '__main__':
-    calc_actions(snapfile=sys.argv[1])
+    calc_actions(snapfile=sys.argv[1],first=int(sys.argv[2]))
